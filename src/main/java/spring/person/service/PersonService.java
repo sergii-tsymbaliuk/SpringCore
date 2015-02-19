a68bd36033041a34668e547a6bc53d8cfe20ac45
Sergii_Tsymbaliuk <Sergii_Tsymbaliuk@EPUAKYIW1562.kyiv.epam.com>
2015-02-19 17:13:32 +0200

    tst

--- src/main/java/spring/person/service/PersonService.java
+++ src/main/java/spring/person/service/PersonService.java
@@ -4,18 +4,23 @@ import java.util.Date;
 
 import javax.annotation.Resource;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import spring.person.dao.PersonDao;
 import spring.person.model.Person;
 
 
+@Scope(value="request", proxyMode = ScopedProxyMode.TARGET_CLASS ) 
 @Service
 public class PersonService {
 	
 	@Resource
 	PersonDao personDao;
 
+	@Transactional
 	public <S extends Person> S save(S entity) {
 		if (entity.getCreateDate() == null) 
 			entity.setCreateDate(new Date());
@@ -46,20 +51,30 @@ public class PersonService {
 		return personDao.count();
 	}
 
+	@Transactional
 	public void delete(Long id) {
 		personDao.delete(id);
 	}
 
+	@Transactional
 	public void delete(Person entity) {
 		personDao.delete(entity);
 	}
 
+	@Transactional
 	public void delete(Iterable<? extends Person> entities) {
 		personDao.delete(entities);
 	}
 
+	@Transactional
 	public void deleteAll() {
 		personDao.deleteAll();
 	}
 
+	public PersonService() {
+		super();
+		System.out.println("new PersonService() ");
+	}
+
+	
 }
--- src/main/java/spring/person/web/PersonController.java
+++ src/main/java/spring/person/web/PersonController.java
@@ -1,26 +1,38 @@
 package spring.person.web;
 
 
+import java.util.Date;
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import spring.person.model.Person;
 
+@Scope() //("session") //Singleton for session/request/server
 @Controller
 @RequestMapping("person")
+//@SessionAttributes("test")
 public class PersonController extends PersonAbstractController {
 
 		@RequestMapping("view")
-		public String showViewForm(){
+		public String showViewForm(Model model){
+			model.addAttribute("test", new Date());
 			return "person/view";
 		}
 		
 		@RequestMapping("")
 		public String showPersonList(Model model){
+			System.out.println("PC: "+ this);			
+			System.out.println("PC.class: "+ this.getClass());			
+			System.out.println("Service= "+ service);
+			System.out.println("Service.class = "+ service.getClass());
+			
 			model.addAttribute("personList", service.findAll());
 			return "person/list";
 		}
@@ -53,4 +65,11 @@ public class PersonController extends PersonAbstractController {
 			return person == null ? new Person() : person;
 			
 		}
+
+		public PersonController() {
+			super();
+			System.out.println("new PersonController()");
+		}
+		
+		
 }
